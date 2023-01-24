ec5d53be4d99c27a720f2b3d7b7df09798e4cccf
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-01-24 12:45:51 -0600

    Add input error handling

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ for (let i = 0; i < process.argv.length; i++) {
     paymentInput = process.argv[i + 1];
   }
 }
+
+if (itemCostInput == null) {
+  console.error("--item-cost must be provided");
+  process.exit(1);
+}
+
+const itemCost = Number(itemCostInput) * 100;
+if (isNaN(itemCost)) {
+  console.log("--item-cost must be a number");
+  process.exit(1);
+}
+
+if (paymentInput == null) {
+  console.error("--payment must be provided");
+  process.exit(1);
+}
+
+const payment = Number(paymentInput) * 100;
+if (isNaN(payment)) {
+  console.log("--payment must be a number");
+  process.exit(1);
+}
+
+console.log(itemCost, payment)
